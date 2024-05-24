802a14d687a086b1b33daa935db784a58d342399
OsamuMurataNeec <murataosa@gmail.com>
2024-05-24 15:19:53 +0900

    0524変更

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
 #include <list>
 
@@ -6,15 +7,36 @@ using namespace std;
 
 int main() {
 	
-	list<const char*>rosen = {"田端","日暮里","西日暮里"};
+	list<const char*>rosen =
+	   {"komagome","tabata","nippori"};
 
+	for (auto itr = rosen.begin();
+		itr != rosen.end();
+		itr++)
+	{
+		cout << *itr << endl;
+	}
+
+	cout << endl;
+
+
+	for (list<const char*>::iterator itr = rosen.begin();
+		itr != rosen.end(); itr++) {
+		if (strcmp(*itr, "nippori") == 0) {
+			itr = rosen.insert(itr, "nishi_nippori");
+			++itr;
+		}
+	}
+	
 	for (list<const char*>::iterator itr = rosen.begin();
 		itr != rosen.end();
 		itr++)
 	{
 		cout << *itr << endl;
 	}
-		return 0;
+
+
+return 0;
 }
 
 
